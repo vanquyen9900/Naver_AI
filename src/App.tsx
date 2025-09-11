83d0098217a6ccdb5ce941090425f2c2ecc229dc
shibuina <anhddh180559@fpt.edu.vn>
2025-09-11 22:55:54 +0700

    update_data_analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import TasksPage from "./pages/TasksPage";
 import CalendarPage from "./pages/CalendarPage";
+import DataAnalyticsPage from "./pages/DataAnalytics";
 import HistoryPage from "./pages/HistoryPage";
 
 const App: React.FC = () => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Route path="/" element={<AboutPage />} />
         <Route path="/tasks" element={<TasksPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/analytics" element={<DataAnalyticsPage />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </BrowserRouter>
